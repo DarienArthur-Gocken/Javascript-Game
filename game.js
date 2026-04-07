f3dbe8bd26c86ca46611d6cfff7baa8da52c74b4
Omnissiah <234093135+Zeta1313@users.noreply.github.com>
2026-04-07 10:55:49 -0700

    Added upgrades array

--- game.js
+++ game.js
@@ -2,6 +2,12 @@ let bakedGoods = 0;
 
 let goodsPerClick = 1;
 
+const upgrades = [
+    {id: 1, name: "Bigger Oven", cost: 10, bonus: 3},
+    {id: 2, name: "Better Ingredients", cost: 25, bonus: 7},
+    {id: 3, name: "Hire another baker", cost: 50, bonus: goodsPerClick}
+];
+
 function updateDisplay() {
     document.getElementById('score-display').textContent = 'Baked Goods: ' + bakedGoods;
     document.getElementById('rate-display').textContent = 'Goods per Click: ' + goodsPerClick;
